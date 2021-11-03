f03092ace0fe99e373ee791f301c4c508b4d6f0e
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-11-03 19:55:34 +0100

    deleted broken code

--- src/App.js
+++ src/App.js
@@ -16,21 +16,11 @@ class App extends React.Component {
     }
 
     this.handleButtonClick = this.handleButtonClick.bind(this)
-    this.handleButtonClick = this.addItem.bind(this)
 
   }
 
   handleButtonClick(tab) {
     this.setState({ activeTab: tab })
-  }
-
-  addItem(name, price) {
-    const item = {
-      name,
-      price
-    }
-    this.setState({ items: [item, ...this.state.items]})
-    console.log(name, price)
   }
   
   render() {
@@ -53,7 +43,7 @@ class App extends React.Component {
           handleClick={this.handleButtonClick}
           isSelected={this.state.activeTab === "Pay"}
         />
-        {this.state.activeTab === "Add" && <Add addItem={this.addItem}/>}
+        {this.state.activeTab === "Add" && <Add />}
         {this.state.activeTab === "List" && <List />}
         {this.state.activeTab === "Pay" && <Pay />}
       </div>
--- src/components/Add.js
+++ src/components/Add.js
@@ -8,42 +8,24 @@ class Add extends Component {
       name: "",
       price: 0,
     }
-
-    this.handleNameChange = this.handleNameChange.bind(this)
-    this.handlePriceChange = this.handlePriceChange.bind(this)
-  }
-  
-  handleNameChange(e) {
-    this.setState({ name: e.target.value })
-  }
-
-  handlePriceChange(e) {
-    this.setState({ price: e.target.value })
   }
 
   render() {
-    const { addItem } = this.props
-
-    console.log("this.state di Add :", this.state)
-    console.log("this.props di Add :", this.props)
-
     return (
       <div>
         <h1>Add</h1>
         <div className="d-flex align-items-center">
             <input 
               type="text"
-              onChange={this.handleNameChange}
             />
             <input 
               className="mx-4"
               type="range"
               min={1}
               max={10}
-              onChange={this.handlePriceChange}
             />
             <span>{this.state.price}</span>
-            <button className="btn btn-success ms-5" onClick={() => addItem(this.state.name, this.state.price)}>Add</button>
+            <button className="btn btn-success ms-5">Add</button>
         </div>
       </div>
     );
